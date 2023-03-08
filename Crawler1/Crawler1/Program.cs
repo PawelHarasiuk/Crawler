aa77359b71dbe99c2e50feb74a84ff8200ca7411
s24628 <s24628@LAB114-03>
2023-03-08 15:29:20 +0100

    End of the lesson

--- Crawler1/Crawler1/Program.cs
+++ Crawler1/Crawler1/Program.cs
@@ -2,9 +2,22 @@
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            if (args.Length == 0)
+            {
+                throw new ArgumentNullException();
+            }
+
+            string url = args[0];
+
+            using HttpClient client = new();
+            //zawlnianie zasobow: client.Dispose();
+            //ale zamiast można użyć scopa:
+            //using(HttpClient client = new()){}
+            //lub tak using HttpClient client = new();
+            HttpResponseMessage result = await client.GetAsync(url);
+            string content = await result.Content.ReadAsStringAsync();
         }
     }
 }
