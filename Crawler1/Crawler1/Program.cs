ac8e3a96f196e47fd9449d0db7aa639d09455ce9
Pawel Harasiuk <pharasiuk01@gmail.com>
2023-03-14 15:13:44 +0100

    Updated program

--- Crawler1/Crawler1/Program.cs
+++ Crawler1/Crawler1/Program.cs
@@ -1,23 +1,59 @@
-﻿namespace Crawler1
+﻿using System.Text.RegularExpressions;
+
+namespace Crawler1
 {
-    internal class Program
+    class Program
     {
         static async Task Main(string[] args)
         {
-            if (args.Length == 0)
+            if (args.Length != 1)
             {
                 throw new ArgumentNullException();
             }
 
             string url = args[0];
 
+            if (!Uri.TryCreate(url, UriKind.Absolute, out Uri uri))
+            {
+                throw new ArgumentException("Podany argument nie jest poprawnym adresem URL");
+            }
+
             using HttpClient client = new();
-            //zawlnianie zasobow: client.Dispose();
-            //ale zamiast można użyć scopa:
-            //using(HttpClient client = new()){}
-            //lub tak using HttpClient client = new();
+            
             HttpResponseMessage result = await client.GetAsync(url);
+
+            if (!result.IsSuccessStatusCode)
+            {
+                throw new Exception("Błąd w czasie pobierania strony");
+            }
+
             string content = await result.Content.ReadAsStringAsync();
+         
+            HashSet<string> emailSet = FindEmails(content);
+
+            if (emailSet.Count == 0)
+            {
+                throw new Exception("Nie znaleziono adresow email");
+            }
+
+            foreach (string email in emailSet)
+            {
+                Console.WriteLine(email);
+            }
+        }
+
+        static HashSet<string> FindEmails(string html)
+        {
+            Regex emailRegex = new Regex(@"\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b");
+            MatchCollection matches = emailRegex.Matches(html);
+            HashSet<string> emails = new HashSet<string>();
+
+
+            foreach (Match match in matches)
+            {
+                emails.Add(match.Value);
+            }
+            return emails;
         }
     }
 }
